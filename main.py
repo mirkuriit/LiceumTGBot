337847a2d3392a1a68b6b274e5e7b33169b5deaf
devepa <tot-kto-idet@yandex.ru>
2022-09-24 00:08:19 +0800

    wrting new user in db

--- main.py
+++ main.py
@@ -2,35 +2,40 @@ import telebot
 from markups.timetable_markup import markup
 import re
 global timatable_picture
+from repository import TGBotUserRepository
 
 bot = telebot.TeleBot('5730707714:AAEsGA4qxpTvBG0ycqgPZG_KNfTBExL2i-0')
+user_repository = TGBotUserRepository('D:/DBases/liceumTGbot.db')
+
+
+def _is_correct_class(user_class):
+    pattern1011 = r'1[0-1][А-Ва-в]'
+    pattern89 = r'[8-9][А-Ва-в]'
+    if len(user_class) > 3:
+        return False
+    elif len == 3:
+        return bool(re.fullmatch(pattern1011, user_class))
+    else:
+        return bool(re.fullmatch(pattern89, user_class))
 
 
 def get_class(message):
-    print(get_class)
-    if is_correct_class(message.text):
+    print("get_class")
+    user_class = message.text
+    if _is_correct_class(user_class):
+        user_repository.set_user_class(message.from_user.id, message.text)
         bot.send_message(message.from_user.id, "Чего тебе надобно?", reply_markup=markup)
-        #bot.register_next_step_handler(message, choose_action)
     else:
         bot.send_message(message.from_user.id, "Некорректный класс. Попробуйте еще раз")
         bot.register_next_step_handler(message, get_class)
 
 
-def choose_action(message):
-    pass
-    #bot.send_message(message.from_user.id, "Чего тебе надобно?", reply_murkup=markup)
-
-
-def is_correct_class(user_class):
-    if len(user_class) > 3:
-        return False
-    else:
-        if len(user_class) == 3:
-            pattern = r'1[0-1][А-Ва-в]'
-            return True if re.fullmatch(pattern, user_class) else False
-        else:
-            pattern = r'[8-9][А-Ва-в]'
-            return True if re.fullmatch(pattern, user_class) else False
+def get_name(message):
+    print("get_name")
+    user_repository.add_user(message.text, message.from_user.id)
+    bot.send_message(message.from_user.id, "Укажите свой класс в формате ЦифраБуква "
+                                            "(Например, 9А)")
+    bot.register_next_step_handler(message, get_class)
 
 
 @bot.message_handler(commands=['start', 'help', 'reg'])
@@ -43,17 +48,20 @@ def index(message):
         case "/help":
             bot.send_message(message.from_user.id, "Тут будут все команды")
         case "/reg":
-            bot.send_message(message.from_user.id, "Укажите свой класс в формате ЦифраБуква "
-                                                   "(Например, 9А)")
-            bot.register_next_step_handler(message, get_class)
-
+            if user_repository.is_user_exists(message.from_user.id):
+                bot.send_message(message.from_user.id,
+                                 "Вы уже прошли регистрацию. Для изменения профиля используйте команду, которая еще не реализована",
+                                 reply_markup=markup)
+            else:
+                bot.send_message(message.from_user.id, "Введите свое имя")
+                bot.register_next_step_handler(message, get_name)
 
 
 @bot.message_handler(content_types=["text"])
-def get_text_message(message):
+def to_react(message):
     print("get_text_message")
     if message.text.lower() == "дай расписание шаболда":
-        timatable_picture = open("timetable.jpg", "rb")
+        timatable_picture = open("resourses/timetable.jpg", "rb")
         bot.send_photo(message.from_user.id, timatable_picture)
 
 
---
+++ repository.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+
+class TGBotUserRepository:
+    def __init__(self, db_file):
+        self.connection = sqlite3.connect(db_file, check_same_thread=False)
+        self.cursor = self.connection.cursor()
+
+    def is_user_exists(self, user_id):
+        db_request = self.cursor.execute("SELECT name FROM users WHERE user_id = ?", (user_id,))
+        return bool(len(db_request.fetchall()))
+
+    def get_user_name_by_id(self, user_id):
+        db_request = self.cursor.execute("SELECT name FROM users WHERE 'user_id' = ?", (user_id,))
+        return db_request.fetchone()[0]
+
+    def add_user(self, user_name, user_id, user_class=None):
+        self.cursor.execute("INSERT INTO users (user_id, class,name) VALUES (?, ?, ?)",
+                            (user_id, user_class, user_name))
+        return self.connection.commit()
+
+    def set_user_class(self, user_id, user_class):
+        self.cursor.execute("UPDATE users SET class = ? WHERE user_id = ?", (user_class, user_id))
+        return self.connection.commit()
+
+    def set_user_name(self, user_id):
+        pass
+
+    def _close_db(self):
+        self.connection.close()
