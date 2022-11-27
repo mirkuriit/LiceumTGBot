487c830bd0d2e7ba5192250ee38150b9624bdf67
mirkuriit <tot-kto-idet@yandex.ru>
2022-11-27 19:11:07 +0800

    get set user school, rework from class to class_id

--- repository.py
+++ repository.py
@@ -14,17 +14,25 @@ class TGBotUserRepository:
         db_request = self.cursor.execute("SELECT name FROM users WHERE user_id = ?", (user_id,))
         return db_request.fetchone()[0]
 
-    def get_user_class_by_id(self, user_id):
-        db_request = self.cursor.execute("SELECT class FROM users WHERE user_id = ?", (user_id,))
+    def get_user_class_id_by_id(self, user_id):
+        db_request = self.cursor.execute("SELECT class_id FROM users WHERE user_id = ?", (user_id,))
         return db_request.fetchone()[0]
 
-    def add_user(self, user_name, user_id, user_class=None):
-        self.cursor.execute("INSERT INTO users (user_id, class,name) VALUES (?, ?, ?)",
-                            (user_id, user_class, user_name))
+    def get_user_school_id_by_id(self, user_id):
+        db_request = self.cursor.execute("SELECT school_id FROM users WHERE user_id = ?", (user_id,))
+        return db_request.fetchone()[0]
+
+    def add_user(self, user_name, user_id, user_class_id=None, user_school_id=None):
+        self.cursor.execute("INSERT INTO users (user_id, name, class_id,  school_id) VALUES (?, ?, ?, ?)",
+                            (user_id, user_name, user_class_id, user_school_id))
         return self.connection.commit()
 
-    def set_user_class(self, user_id, user_class):
-        self.cursor.execute("UPDATE users SET class = ? WHERE user_id = ?", (user_class, user_id))
+    def set_user_school(self, user_id, user_school_id):
+        self.cursor.execute("UPDATE users SET school_id = ? WHERE user_id = ?", (user_school_id, user_id))
+        return self.connection.commit()
+
+    def set_user_class(self, user_id, user_class_id):
+        self.cursor.execute("UPDATE users SET class_id = ? WHERE user_id = ?", (user_class_id, user_id))
         return self.connection.commit()
 
     def set_user_name(self, user_id, user_name):
